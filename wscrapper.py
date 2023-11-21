469f789f85384405e1fccec6e96840af2ce09fb2
wbpatterson <wbpatterson@aggies.ncat.edu>
2023-11-20 19:37:17 -0500

    updated comments

--- wscrapper.py
+++ wscrapper.py
@@ -4,20 +4,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 import pyshorteners
 import csv
 import re
-import time
 
-start = time.time()
 
-# algorithm needs to take in filtered searches (i.e, #guest, location) from airbnb for best results
-# need to prioritize improving time 
-        
 def shorten_url(long_url):
     # Initialize the Shortener class with the desired URL shortening service
     s = pyshorteners.Shortener()
@@ -26,9 +21,11 @@ def shorten_url(long_url):
 
     return shortened_url
 
+# main page scraping function that retrieves page HTML
 def scrape(url):
     driver.get(url)
     get_url = driver.current_url
+    # VITAL: page must wait till div below is loaded as HTML will not be properly loaded otherwise
     wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'l1ovpqvx.c1ytbx3a.dir.dir-ltr')))
     
     if get_url == url: 
@@ -37,16 +34,16 @@ def scrape(url):
     soup = BeautifulSoup(page_source,features='lxml')
     return soup
 
-listing_arr = []
+listing_arr = [] # list that will store listings
 
-def placeListings(listings, links, time):
+def placeListings(listings, links, date):
     
     for n in range(len(listings)):
         info = listings[n].contents
         location = info[0].text
         description = info[1].text
         bedrooms = info[2].text      
-        dates = time
+        dates = date
         price_per_night = info[3].text
         link = shorten_url('https://www.airbnb.com'+links[n]['href'])
 
@@ -75,7 +72,6 @@ def placeListings(listings, links, time):
 
 
 if __name__=="__main__":
-    
     options = Options()
     options.headless = True
     options.add_argument("--headless=new")
@@ -91,24 +87,31 @@ if __name__=="__main__":
     if url == None:
         print("invalid url")
         exit()
-    
+
     for page in range(num_pages):
         
         if url == None:
             break
-        
-        curr_site = scrape(url) # assigns new page to be scraped and updates driver
-        listings = curr_site.find_all('div','g1qv1ctd c1v0rf5q dir dir-ltr') # finds all listings 
+       
+        # assigns new page to be scraped and updates driver
+        curr_site = scrape(url) 
+        # finds all listings on a page
+        listings = curr_site.find_all('div','g1qv1ctd c1v0rf5q dir dir-ltr')
+        # finds all listings links on a page
         links = curr_site.find_all('a', 'l1ovpqvx bn2bl2p dir dir-ltr')
+        # finds all dates for listings 
         date = curr_site.find_all('div', 'f16sug5q dir dir-ltr')[1].text
-        placeListings(listings, links, date) # places listings into list
+        # places listings into list
+        placeListings(listings, links, date)
+        # updates page to be searched via scrollbar near the bottom of the page
         url = driver.find_element(By.CLASS_NAME, 'l1ovpqvx.c1ytbx3a.dir.dir-ltr').get_attribute("href")
     
 
+    # returns price as an integer excluding $
     def cmp_list(listing):
         return int(listing['price_per_night'][1:])
 
-    listing_arr.sort(key=cmp_list)
+    listing_arr.sort(key=cmp_list)  # sorts listings in ascending price order
 
     fields = ['link', 'location', 'owner/description', 'bedrooms/amenities', 'dates', 'price_per_night', 'rating/reviews']
     file = open('airbnb.csv', 'w')
@@ -119,6 +122,3 @@ if __name__=="__main__":
     # close webdriver
     driver.quit()
 
-    end = time.time()
-
-    print(end-start)
